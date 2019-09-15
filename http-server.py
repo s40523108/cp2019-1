9fa3d1b9558537f6ac2ba3798ae8a17fb16dbad5
mdecourse <mdecourse@gmail.com>
2019-09-15 11:57:19 +0800

    add http-server for sending CORS header

--- http-server.py
+++ http-server.py
@@ -6,17 +6,29 @@ import http.server, ssl
 def domake():
     # build directory
     #os.chdir("./../")
-    server_address = ('localhost', 8444)
+    server_address = ('localhost', 8000)
     httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
     httpd.socket = ssl.wrap_socket(httpd.socket,
                                    server_side=True,
-                                   certfile='./cmsimde/localhost.crt',
-                                   keyfile='./cmsimde/localhost.key',
+                                   certfile='./localhost.crt',
+                                   keyfile='./localhost.key',
                                    ssl_version=ssl.PROTOCOL_TLSv1)
     print(os.getcwd())
-    print("8444 https server started")
+    print("8000 https server started")
     httpd.serve_forever()
 
 # 利用執行緒執行 https 伺服器
 make = threading.Thread(target=domake)
 make.start()
+
+
+from http.server import HTTPServer, SimpleHTTPRequestHandler, test
+import sys
+
+class CORSRequestHandler (SimpleHTTPRequestHandler):
+    def end_headers (self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        SimpleHTTPRequestHandler.end_headers(self)
+
+if __name__ == '__main__':
+    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
