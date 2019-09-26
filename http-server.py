e24dbc697dd1113a75933300b0eb8dfee962fe1a
mdecourse <mdecourse@gmail.com>
2019-09-26 13:44:03 +0800

    modified 3112 links

--- http-server.py
+++ http-server.py
@@ -6,29 +6,17 @@ import http.server, ssl
 def domake():
     # build directory
     #os.chdir("./../")
-    server_address = ('localhost', 8000)
+    server_address = ('localhost', 8444)
     httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
     httpd.socket = ssl.wrap_socket(httpd.socket,
                                    server_side=True,
-                                   certfile='./localhost.crt',
-                                   keyfile='./localhost.key',
+                                   certfile='./cmsimde/localhost.crt',
+                                   keyfile='./cmsimde/localhost.key',
                                    ssl_version=ssl.PROTOCOL_TLSv1)
     print(os.getcwd())
-    print("8000 https server started")
+    print("8444 https server started")
     httpd.serve_forever()
 
 # 利用執行緒執行 https 伺服器
 make = threading.Thread(target=domake)
 make.start()
-
-
-from http.server import HTTPServer, SimpleHTTPRequestHandler, test
-import sys
-
-class CORSRequestHandler (SimpleHTTPRequestHandler):
-    def end_headers (self):
-        self.send_header('Access-Control-Allow-Origin', '*')
-        SimpleHTTPRequestHandler.end_headers(self)
-
-if __name__ == '__main__':
-    test(CORSRequestHandler, HTTPServer, port=int(sys.argv[1]) if len(sys.argv) > 1 else 8000)
